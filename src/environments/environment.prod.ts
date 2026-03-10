67286bf8b64eda47eba73fa7e45a99637a46919d
DESKTOP-TTN1AHV\USER <hrant.abrahamian@gmail.com>
2026-03-11 00:25:32 +0400

    improve security and remove unnecessary files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: true,
-  apiKey: 'REDACTED_FIREBASE_KEY',
+  apiKey: 'REPLACE_WITH_FIREBASE_WEB_API_KEY',
   fbDbUrl: 'https://g-blog-708a2-default-rtdb.firebaseio.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'REDACTED_FIREBASE_KEY',
+  apiKey: 'REPLACE_WITH_FIREBASE_WEB_API_KEY',
   fbDbUrl: 'https://g-blog-708a2-default-rtdb.firebaseio.com/'
 };
 
